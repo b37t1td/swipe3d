9fd439e2857d136f158301a7fb0cfffa318f5ab7
linuxenko <linuxenko@yahoo.com>
2016-09-21 14:14:08 +0300

    Pretty exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Swipe from './swipe.js'
 import Slide from './slide.js'
-import {DIR} from './constants.js'
+import Scroll from './scroll.js'
+import {DIR, ACTION} from './constants.js'
+import {ael, rel, grc , grp, ev, isParent} from './tools.js'
 
 export {
   Swipe,
   Slide,
-  DIR
+  Scroll,
+  DIR,
+  ACTION,
+  ael, rel, grc , grp, ev, isParent
 }
-  
